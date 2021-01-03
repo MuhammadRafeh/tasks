b59af897870d876e77b050920c72af17cfb4f44c
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-01-03 21:01:40 +0300

    Completed Task App

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
   const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = enteredGoal => {
-    // console.log(enteredGoal)
+    if (enteredGoal.length === 0) return
     setCourseGoals(courseGoals => [...courseGoals, { id: Math.random().toString(), text: enteredGoal }])
     setIsAddMode(false);
   }
@@ -28,8 +28,12 @@ export default function App() {
 
   return (
     <View style={styles.screen}>
-      <Button title="Add New Goal" onPress={() => {setIsAddMode(true)}}/>
-      <GoalInput resetAddMode={resetAddMode} visible={isAddMode} addGoalHandler={addGoalHandler} />
+      <Button title="Add New Goal" onPress={() => { setIsAddMode(true) }} />
+      <GoalInput
+        resetAddMode={resetAddMode}
+        visible={isAddMode}
+        addGoalHandler={addGoalHandler}
+      />
       <FlatList
         showsVerticalScrollIndicator={false}
         data={courseGoals}
@@ -37,56 +41,12 @@ export default function App() {
           <GoalItem
             id={item.id}
             onDelete={removeGoalHandler}
-            text={item.text}/>}
+            text={item.text} />}
         keyExtractor={item => item.id}
       />
     </View>
   );
 }
-
-// export default function App() {
-//   return (
-//     <View style={{paddingTop: 30, flex: 1, backgroundColor: 'black', flexDirection: 'column', alignItems: 'flex-start'}}>
-//       <View
-//         style={{
-//           backgroundColor: 'red',
-//           justifyContent: 'center',
-//           alignSelf: 'stretch',
-//           // flex: 1, //take space along main axes
-//           // width: 100,
-//           height: 100,
-//           alignItems: 'center'}}
-//       >
-//         <Text>1</Text>
-//       </View>
-//       <View 
-//         style={{
-//           // flex: 1,
-//           backgroundColor: 'orange',
-//           justifyContent: 'center',
-//           height: 100,
-//           // flex: 1,
-//           width: 150,
-//           // alignSelf: 'flex-end',
-//           alignItems: 'center'}}
-//       >
-//         <Text>2</Text>
-//       </View>
-//       <View 
-//         style={{
-//           backgroundColor: 'green',
-//           justifyContent: 'center',
-//           height: 100,
-//           // flex: 3,
-//           // alignSelf: 'center',
-//           width: 100,
-//           alignItems: 'center'}}
-//       >
-//         <Text>3</Text>
-//       </View>
-//     </View>
-//   );
-// }
 
 const styles = StyleSheet.create({
   screen: {
